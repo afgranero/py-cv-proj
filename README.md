0979f1b9dd5caf1682e9aeffbbfaf01f0e329f38
Airton da Fonseca Granero <agranero@gmail.com>
2017-10-31 18:08:14 -0200

    More changes to README.md and comments.

--- README.md
+++ README.md
@@ -16,15 +16,17 @@ Detect circles with diameter greater than 10 pixels.
 - I took the following liberties:
     - I used tab as 4 spaces (the default in my environment and PEP 8 recommends);
  - I used virtualenv to isolate libraries;
- - I did not committed the virtual environment to git though, I used pip freeze to create a __requirements.txt__. See this file for the libraries versions needed.
- - I created the switch -debug on the command line, will do two things:
+ - I did not committed the virtual environment to git though, I used `pip freeze` to create a `requirements.txt`.
+ - refer to `requirements.txt` for the libraries versions needed;
+ - I created the switch `-debug` on the command line, will do two things:
          - show the intermediate states of processing on screen
-         - save the intermediate images with same name as the original as prefix and with suffix _1, _2, etc
+         - save the intermediate images with same name as the original as prefix and with suffix `_1`, `_2`, etc
          - on a production environment I would separate this on another class using python decorators to call it
          - if there were more test cases I would not print them individually, I would just save the steps on disk
 
 **Usage:**
-```python pycv-proj-test.py images/image_filename
+```
+python pycv-proj-test.py images/image_filename
 python3 pycv-proj-test.py images/image_filename
 python pycv-proj-test.py images/image_filename -nodebug
 ```
--- pycv-proj-test.py
+++ pycv-proj-test.py
@@ -20,11 +20,11 @@
 # - I took the following liberties:
 #     - I used tab as 4 spaces (the default in my environment and PEP 8 recommends);
 #  - I used virtualenv to isolate libraries;
-#  - I did not committed the virtual environment to git though, I used pip freeze to create a __requirements.txt__;
-#  - refer to __requirements.txt__ for the libraries versions needed;
-#  - I created the switch -debug on the command line, will do two things:
+#  - I did not committed the virtual environment to git though, I used `pip freeze` to create a `requirements.txt`;
+#  - refer to `requirements.txt` for the libraries versions needed;
+#  - I created the switch `-debug` on the command line, will do two things:
 #          - show the intermediate states of processing on screen
-#          - save the intermediate images with same name as the original as prefix and with suffix _1, _2, etc
+#          - save the intermediate images with same name as the original as prefix and with suffix `_1`, `_2`, etc
 #          - on a production environment I would separate this on another class using python decorators to call it
 #          - if there were more test cases I would not print them individually, I would just save the steps on disk
 
@@ -33,7 +33,6 @@
 #   python pycv-proj-test.py images/image_filename
 #   python3 pycv-proj-test.py images/image_filename
 #   python pycv-proj-test.py images/image_filename -nodebug
-
 
 import numpy as np
 import cv2
@@ -141,7 +140,6 @@ class FindCircles():
     def _save_images(self):
         # TODO save all images on self.debug_images
         pass
-
 
 
 def main():
