d9871faf96d1e96fcc7494c401eeadbb85dc8efb
Airton da Fonseca Granero <agranero@gmail.com>
2017-10-31 14:46:08 -0200

    first attempt with partial success

--- pycv-proj-test.py
+++ pycv-proj-test.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-# Author; Airton da Fonseca Granero
+# Author: Airton da Fonseca Granero
 
 # Developed and tested with PyCharm Community Edition 2017:
 #
@@ -11,18 +11,23 @@
 #
 
 # Notes:
+#   - I pulled this to https://github.com/afgranero/py-cv-proj
+#   - I have other repos but they are private tell me if you want access to them
 #   - As this is a test and I am doing it alone I will not use topic branches or issues
 #   - I interpreted "detect circles" in two ways:
 #       - find circles centers
 #       - highlight the circles on the original image and save it
 #   - I took the following liberties:
 #       - I used tab as 4 spaces (the default in my environment and PEP 8 recommended)
+#       - I used virtualenv to isolate libraries
+#       - I did not committed the virtual environment to git though, I used pip freeze to create a requiremtns.txt
 #       - the switch -debug on the command line, will do two things:
 #           - show the intermediate states of processing on screen
-#           - save the intermediate images with same name as the original as prefix and with  sufix _1, _2, etc
+#           - save the intermediate images with same name as the original as prefix and with suffix _1, _2, etc
 #           - on a production environment I would separate this on another class using python decorators to call it
-##   -
-# Use:
+#           - if there were more test cases I would not print them individually but just save the steps on disk
+
+# Usage:
 #
 #   python pycv-proj-test.py images/image_filename
 #   python3 pycv-proj-test.py images/image_filename
@@ -45,25 +50,55 @@ class Finder():
             self.debug_images_current = 0
 
     def find_circle(self):
-        self._show_step(0)
 
-        self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
-        self._show_step(1)
+        img = np.array(self.img, copy=True)
 
-        self.img = self.clean()
-        self._show_step(2)
+        self._show_step(0, img)
 
-    def clean(self):
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        self._show_step(1, img)
+
+        img = self.clean(img)
+        self._show_step(2, img)
+
+        img = self.highlight_circles(img)
+        self._show_step(3, img)
+
+    def clean(self, img):
         kernel = np.ones((3, 3), np.uint8)
         # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
-        img = cv2.morphologyEx(self.img, cv2.MORPH_OPEN, kernel, iterations = 3)
+        img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel, iterations = 4)
         return img
 
-    def _show_step(self, n):
+    def highlight_circles(self, img, param2=36):
+        # TO DO separate detect and highlight
+        min_param2 = 18
+
+        circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 20, param1=150, param2=param2, minRadius=10, maxRadius=500)
+
+        if circles is not None:
+            circles = np.uint16(np.around(circles))
+            blue = (255, 0, 0)
+            red = (0, 0, 255)
+            for i in circles[0, :]:
+                cv2.circle(self.img, (i[0], i[1]), i[2], blue, 2) # show circle as a green circle
+                cv2.circle(self.img, (i[0], i[1]), 2, red, 3) # show centers as a red 2 pixel diameter circle
+            return self.img
+        else:
+            # no circle found use smaller param2 do it just once to avoid find too many circles and too many recursion
+            # the easiest way if to set a min value to param2
+            if param2 > min_param2:
+                param2 /= 2
+                return self.highlight_circles(img, param2)
+            else:
+                return self.img
+
+
+    def _show_step(self, n, img):
         if self.debug:
             self.debug_images_current = n
             self.debug_images_count += 1
-            self.debug_images.append(self.img)
+            self.debug_images.append(img)
             key = 0
             while True:
                 cv2.destroyAllWindows()
